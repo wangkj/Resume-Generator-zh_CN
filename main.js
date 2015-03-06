5a29af993fb80b1e4e50376d17e9305ce316743a
Mitch Samuels <mitch@mitchs.co>
2015-03-06 02:43:24 -0600

    MUCH more efficient. Basically rebuilt. Local storage. Logo. Progress bar. Design tweaks.

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $( document ).ready(function() {
 
 	loadLocal();
 
-	checkStep();
 	//Statement Character Count
 	var text_max = 800;
     $('#statement_remaining').html(text_max);
@@ -16,169 +15,235 @@ $( document ).ready(function() {
 
 
 
+	if (localStorage.dataSavedName) {
+	    $("#intro_reset").show();
+		$("#intro_begin").html("Continue Editing <i class='ion-arrow-right-c'></i>");
+	}
+
 
 });
+
+
+//Reset local storage if it exists.
+$("#intro_reset").click(function(){
+	localStorage.clear();
+	location.reload();
+})
+
 
 //Branding link
 $("#branding").click(function(){
 	window.open("http://mitchs.co");
 })
 
-//Close Intro
-$("#close_intro").click(function(){
-	$("#intro_inner").animate({
-	    marginTop: "-110%"
-	  }, 500);
-	$("#intro_wrapper").delay(500).fadeOut(300);
-})
 
-var stepcounter = 1;
 
-$('#personal-name-input, #personal-email-input, #personal-phone-input, #personal-location-input, #personal-link-input ').bind('keyup', function(e) {
+//Detect Enter on some inputs
+$('#personal_name_input, #personal_email_input, #personal_phone_input, #personal_location_input, #personal_link_input, #skillsinput ').bind('keyup', function(e) {
     if ( e.keyCode === 13 ) { // 13 is enter key
-	    $("#personal_continue").click();
+	    $("#next_button").click();
     }
 });
 
 
-$("#info_bar").click(function(){
-	$("#intro_wrapper").fadeIn(300);
-	$("#intro_inner").delay(100).animate({
-	    marginTop: "75px"
-	  }, 500);
-})
+//Arrow key function
+$(document).keydown(function(e){
+    if (e.keyCode == 37){
+    	if(navCounter>0){
+	    	saveFields();
+	    	$("aside > ul > li").removeClass("activated");
+			$(".hideall, #intro_box, #body_wrap, #work, #schooling, #skills, #statement, #settings, #finished").hide();
+			$("#intro_box").toggleClass("pulse, pulse2");
+			$("#body_wrap").toggleClass("pulse, pulse2");
+	    	navCounter--;
+	    	checkNavPos();
+	    }
+    }
+    if (e.keyCode == 39){
+    	if(navCounter<10){
+	    	saveFields();
+	    	$("aside > ul > li").removeClass("activated");
+			$(".hideall, #intro_box, #body_wrap, #work, #schooling, #skills, #statement, #settings, #finished").hide();
+			$("#intro_box").toggleClass("pulse, pulse2");
+			$("#body_wrap").toggleClass("pulse, pulse2");
+	    	navCounter++;
+	    	checkNavPos();
+	    }
+    }
+});
 
-$(".next").click(function(){
+
+//Create empty variables
+user_job_location = ""; user_job_time = ""; user_emp_desc = ""; user_job_title = ""; user_job_desc = ""; user_employer2 = ""; user_job_location2 = ""; user_job_time2 = ""; user_emp_desc2 = ""; user_job_title2 = "";user_job_desc2 = ""; user_employer3 = ""; user_job_location3 = ""; user_job_time3 = ""; user_emp_desc3 = ""; user_job_title3 = "";user_job_desc3 = ""; user_degree = ""; user_school = ""; user_school_location = ""; user_school_grad = ""; user_degree2 = ""; user_school2 = ""; user_school_location2 = ""; user_school_grad2 = ""; user_degree3 = ""; user_school3 = ""; user_school_location3 = ""; user_school_grad3 = "";
+
+
+//Nav tab control
+$("#tab_intro").click(function(){navCounter=0;})
+$("#tab_1").click(function(){navCounter=1;})
+$("#tab_2").click(function(){navCounter=6;})
+$("#tab_3").click(function(){navCounter=7;})
+$("#tab_4").click(function(){navCounter=8;})
+$("#tab_5").click(function(){navCounter=9;})
+$("#tab_6").click(function(){navCounter=10;})
+$("aside > ul > li").click(function(){
 	saveFields();
-	stepcounter++;
-	console.log(stepcounter);
-	checkStep();
+	$("section").toggleClass("pulse, pulse2");
+	$("aside > ul > li").removeClass("activated");
+	$(".hideall, #intro_box, #body_wrap, #work, #schooling, #skills, #statement, #settings, #finished").hide();
+	checkNavPos();
 })
 
-function checkStep(){
+// New nav controller shit
+navCounter = 0;
+$("#intro_begin").click(function(){
+	navCounter++;
+	checkNavPos();
 	$("aside > ul > li").removeClass("activated");
-	if(stepcounter<=6){
-		$("#personal_information").show();
-		
-		$("#personal_information").toggleClass("pulse, pulse2");
-		$(".personal_block > input").hide();
+	$("#tab_1").addClass("activated");
+})
+
+$("#next_button").click(function(){
+	navCounter++;
+	$("aside > ul > li").removeClass("activated");
+	$(".hideall, #intro_box, #body_wrap, #work, #schooling, #skills, #statement, #settings, #finished").hide();
+	$("#intro_box").toggleClass("pulse, pulse2");
+	$("#body_wrap").toggleClass("pulse, pulse2");
+	checkNavPos();
+	saveFields();
+})
+
+function checkNavPos(){
+	if(10>navCounter>0){
+		$("#body_wrap").show();
+	}
+	if(navCounter==0){
+		$(".hideall, #intro_box, #body_wrap, #work, #schooling, #skills, #statement, #settings, #finished").hide();
+		$("#intro_box").show();
+		$("#tab_intro").addClass("activated");
+		$("#progressbar").val(0);
+	}else if(navCounter==1){
+		$("#intro_box").hide();
+		$("#body_wrap").show();
+		$("#instructions").html("Starting off easy, what's your <b>full name</b>?");
+		$("#personal_name_input").show().focus();
 		$("#tab_1").addClass("activated");
-	}
-	if(stepcounter==1){
-		// $(".personal_block > input").hide();
-		$("#personal-name-input").show().focus();
-		$("#personal_desc").html("Hey there, what's your full <b>name</b>?");
-	}else if(stepcounter==2){
-		// $("#personal_information").show();
-		$("#personal-email-input").show().focus();
-		$("#personal_desc").html("Nice to meet you, "+splitname[0]+"!<br/>What is your <b>email address</b>?");
-	}else if(stepcounter==3){
-		$("#personal-phone-input").show().focus();
-		$("#personal_desc").html("I know we just met, but can I have your <b>phone number</b>?");
-	}else if(stepcounter==4){
-		$("#personal-location-input").show().focus();
-		$("#personal_desc").html("This might sound creepy, but I need your <b>location</b>.");
-	}else if(stepcounter==5){
-		$("#personal-link-input").show().focus();
-		$("#personal_desc").html("Lastly, do you have a <b>Linkedin or portfolio</b> link.");
-	}else if(stepcounter==6){
-		$("#personal_desc").html("Alrighty!<br/><br/>To recap, you are <b>"+user_name+"</b> from <b>"+user_location+"</b> who can be emailed at <b>"+user_email+"</b> or called at <b>"+user_phone+"</b>. Finally, Your online portfolio can be seen at <b>"+user_link+"</b>");
-	}else if(stepcounter==7){
-		$("#tab_2").addClass("activated");
-		$("#personal_information").hide();
-		$("#personal_statement").show();
-		$("#personal_statement").toggleClass("pulse, pulse2");
+		$("#progressbar").val(3);
+	}else if(navCounter==2){
+		$("#instructions").html("This one is tougher, What is your <b>email address</b>?");
+		$("#personal_email_input").show().focus();
+		$("#tab_1").addClass("activated");
+		$("#progressbar").val(6);
+	}else if(navCounter==3){
+		$("#instructions").html("I know we just met, but can I have your <b>number</b>?");
+		$("#personal_phone_input").show().focus();
+		$("#tab_1").addClass("activated");
+		$("#progressbar").val(9);
+	}else if(navCounter==4){
+		$("#instructions").html("Not to be creepy but I need your <b>location</b> too.");
+		$("#personal_location_input").show().focus();
+		$("#tab_1").addClass("activated");
+		$("#progressbar").val(12);
+	}else if(navCounter==5){
+		$("#instructions").html("Do you have a <b>portfolio or linkedin</b> or something?");
+		$("#personal_link_input").show().focus();
+		$("#tab_1").addClass("activated");
+		$("#progressbar").val(15);
+	}else if(navCounter==6){
+		$("#instructions").html("Would you like to provide a <b>Personal statement</b>?<br/>I wouldn't judge if you don't.");
+		$("#statement").show();
 		$("#personal_statement_input").focus();
-	}else if(stepcounter==8){
+		$("#tab_2").addClass("activated");
+		$("#progressbar").val(18);
+	}else if(navCounter==7){
+		$("#instructions").html("If you've had a <b>job</b> you should probably add it here.");
+		$("#work").show();
+		$("#employer").focus();
 		$("#tab_3").addClass("activated");
-		$("#personal_statement").hide();
-		$("#work_experience").show();
-		$("#work_experience").toggleClass("pulse, pulse2");
-		$("#employer").focus()
-	}else if(stepcounter==9){
-		$("#tab_4").addClass("activated");
-		$("#work_experience").hide();
-		$("#education").show();
-		$("#education").toggleClass("pulse, pulse2");
+		$("#progressbar").val(42);
+	}else if(navCounter==8){
+		$("#instructions").html("'Is our children learning?' - G.W. Bush");
+		$("#schooling").show();
 		$("#degree").focus();
-	}else if(stepcounter==10){
-		$("#tab_5").addClass("activated");
-		$("#education").hide();
+		$("#tab_4").addClass("activated");
+		$("#progressbar").val(67);
+	}else if(navCounter==9){
+		$("#instructions").html("Can you actually do anything?");
 		$("#skills").show();
-		$("#skills").toggleClass("pulse, pulse2");
 		$("#skillsinput").focus();
-	}else if(stepcounter==11){
-		$("#tab_6").addClass("activated");
-		$("#skills").hide();
+		$("#tab_5").addClass("activated");
+		$("#progressbar").val(90);
+	}else if(navCounter==10){
 		$("#settings").show();
-		$("#settings").toggleClass("pulse, pulse2");
-	}else if(stepcounter==12){
-		$("#tab_7").addClass("activated");
-		$("#settings").hide();
-		$("#finished").show();
-		$("#finished").toggleClass("pulse, pulse2");
-	}
-	else{
-		alert("error.");
+		$("#tab_6").addClass("activated");
+		$("#progressbar").val(100);
 	}
 }
 
 
 //Add job control
 jobnum = 1;
-$("#add_job").click(function(){
+$("#new_job").click(function(){
 	saveFields();
 	$("#employer, #job_location, #job_time, #employer_desc, #title, #job_desc").val("");
+	$("#employer").focus();
 	if(jobnum==1){
-		$("#work_header").text("Alright, do it again for Job 2");
+		$("#instructions").text("You know the drill. Second job This time.");
 	}else if(jobnum==2){
-		$("#work_header").text("I'm limiting you to 3. Nobody cares about the rest.");
-		$("#add_job").prop("disabled",true);
+		$("#instructions").text("I'm limiting you to 3. Nobody cares about the rest.");
+		$("#new_job").prop("disabled",true);
 	}
 	 $("article").animate({ scrollTop: 0 }, "slow");
 	 jobnum++;
 })
+
+
 //Add school control
 schoolnum = 1;
-$("#add_school").click(function(){
+$("#new_school").click(function(){
 	saveFields();
 	$("#degree, #school, #school_location, #graduation").val("");
+	$("#degree").focus();
 	if(schoolnum==1){
-		$("#school_header").text("You better not be putting your high school.");
+		$("#instructions").text("You better not be putting your high school.");
 	}else if(schoolnum==2){
-		$("#school_header").text("Seriously? Three schools? Okay, einstein.");
-		$("#add_school").prop("disabled",true);
+		$("#instructions").text("Seriously? Three schools? Okay, einstein.");
+		$("#new_school").prop("disabled",true);
 	}
 	 $("article").animate({ scrollTop: 0 }, "slow");
 	 schoolnum++;
 })
 
-// NAV CONTROL
-$("aside > ul > li").click(function(){
-	$(this).addClass("activated").siblings().removeClass("activated");
-	$("section").hide();
+
+// Final button, generate PDF
+$("#generate").click(function(){
+	if ($('#serifyes').is(':checked')) {
+		$("#print_page").css("font-family","\'sans-serif\'");
+	}else {
+		$("#print_page").css("font-family","\'Lato\'");
+	}
+	generatePDF();
+	$("#settings").hide();
+	$("#thanks").show();
 })
-$("#tab_1").click(function(){stepcounter=1; checkStep();})
-$("#tab_2").click(function(){stepcounter=7; checkStep();})
-$("#tab_3").click(function(){stepcounter=8; checkStep();})
-$("#tab_4").click(function(){stepcounter=9; checkStep();})
-$("#tab_5").click(function(){stepcounter=10; checkStep();})
-$("#tab_6").click(function(){stepcounter=11; checkStep();})
-$("#tab_7").click(function(){stepcounter=12; checkStep();})
+
+//After generation, return to editing
+$("#backtoedit").click(function(){
+	navCounter =  0;
+	$("aside > ul > li").removeClass("activated");
+	$("#thanks").hide();
+	checkNavPos();
+})
 
 
 
 
 
 function saveFields(){
-
-
-	user_name = $("#personal-name-input").val();
-	splitname = user_name.split(" ");
-	user_email = $("#personal-email-input").val();
-	user_phone = $("#personal-phone-input").val();
-	user_location = $("#personal-location-input").val();
-	user_link = $("#personal-link-input").val();
+	user_name = $("#personal_name_input").val();
+	user_email = $("#personal_email_input").val();
+	user_phone = $("#personal_phone_input").val();
+	user_location = $("#personal_location_input").val();
+	user_link = $("#personal_link_input").val();
 
 	user_statement = $("#personal_statement_input").val();
 
@@ -220,27 +285,60 @@ function saveFields(){
 		user_school_location3 = $("#school_location").val();
 		user_school_grad3 = $("#graduation").val();
 	}
-
 	user_skills = $("#skillsinput").val();
-
 	saveLocal(); 
-
-
 }
 
-$("#settings_continue").click(function(){
-	stepcounter=12; 
-	checkStep();
-})
-$("#generate").click(function(){
-	if ($('#serifyes').is(':checked')) {
-		$("#print_page").css("font-family","\'Lora\'");
-	}else {
-		$("#print_page").css("font-family","\'Lato\'");
-	}
-	generatePDF();
-})
+// Local storage stuff
+function saveLocal(){
+	localStorage.setItem('dataSavedName', user_name);
+	localStorage.setItem('dataSavedEmail', user_email);
+	localStorage.setItem('dataSavedPhone', user_phone);
+	localStorage.setItem('dataSavedLocation', user_location);
+	localStorage.setItem('dataSavedUrl', user_link);
 
+	localStorage.setItem('dataSavedStatement', user_statement);
+
+	localStorage.setItem('dataSavedEmp', user_employer);
+	localStorage.setItem('dataSavedJobLoc', user_job_location);
+	localStorage.setItem('dataSavedJobTime', user_job_time);
+	localStorage.setItem('dataSavedEmpDesc', user_emp_desc);
+	localStorage.setItem('dataSavedJobTitle', user_job_title);
+	localStorage.setItem('dataSavedJobDesc', user_job_desc);
+
+	localStorage.setItem('dataSavedDegree', user_degree);
+	localStorage.setItem('dataSavedSchool', user_school);
+	localStorage.setItem('dataSavedSchoolLoc', user_school_location);
+	localStorage.setItem('dataSavedGrad', user_school_grad);
+
+	localStorage.setItem('dataSavedSkills', user_skills);
+
+}
+function loadLocal(){
+	$("#personal_name_input").val(localStorage.getItem('dataSavedName'));
+	$("#personal_email_input").val(localStorage.getItem('dataSavedEmail'));
+	$("#personal_phone_input").val(localStorage.getItem('dataSavedPhone'));
+	$("#personal_location_input").val(localStorage.getItem('dataSavedLocation'));
+	$("#personal_link_input").val(localStorage.getItem('dataSavedUrl'));
+
+	$("#personal_statement_input").val(localStorage.getItem('dataSavedStatement'));
+
+	$("#employer").val(localStorage.getItem('dataSavedEmp'));
+	$("#job_location").val(localStorage.getItem('dataSavedJobLoc'));
+	$("#job_time").val(localStorage.getItem('dataSavedJobTime'));
+	$("#employer_desc").val(localStorage.getItem('dataSavedEmpDesc'));
+	$("#title").val(localStorage.getItem('dataSavedJobTitle'));
+	$("#job_desc").val(localStorage.getItem('dataSavedJobDesc'));
+
+	$("#degree").val(localStorage.getItem('dataSavedDegree'));
+	$("#school").val(localStorage.getItem('dataSavedSchool'));
+	$("#school_location").val(localStorage.getItem('dataSavedSchoolLoc'));
+	$("#graduation").val(localStorage.getItem('dataSavedGrad'));
+
+	$("#skillsinput").val(localStorage.getItem('dataSavedSkills'));
+}
+
+// Generate the Actual PDF and call print function
 function generatePDF(){
 	$("#pdf_name").html(user_name);
 	$("#pdf_email").html(user_email);
@@ -298,87 +396,3 @@ function generatePDF(){
 
 	window.print();
 }
-
-function saveLocal(){
-	localStorage.setItem('dataSavedName', user_name);
-	localStorage.setItem('dataSavedEmail', user_email);
-	localStorage.setItem('dataSavedPhone', user_phone);
-	localStorage.setItem('dataSavedLocation', user_location);
-	localStorage.setItem('dataSavedUrl', user_link);
-
-	localStorage.setItem('dataSavedStatement', user_statement);
-
-	localStorage.setItem('dataSavedEmp', user_employer);
-	localStorage.setItem('dataSavedJobLoc', user_job_location);
-	localStorage.setItem('dataSavedJobTime', user_job_time);
-	localStorage.setItem('dataSavedEmpDesc', user_emp_desc);
-	localStorage.setItem('dataSavedJobTitle', user_job_title);
-	localStorage.setItem('dataSavedJobDesc', user_job_desc);
-
-	localStorage.setItem('dataSavedDegree', user_degree);
-	localStorage.setItem('dataSavedSchool', user_school);
-	localStorage.setItem('dataSavedSchoolLoc', user_school_location);
-	localStorage.setItem('dataSavedGrad', user_school_grad);
-
-	localStorage.setItem('dataSavedSkills', user_skills);
-
-}
-function loadLocal(){
-	$("#personal-name-input").val(localStorage.getItem('dataSavedName'));
-	$("#personal-email-input").val(localStorage.getItem('dataSavedEmail'));
-	$("#personal-phone-input").val(localStorage.getItem('dataSavedPhone'));
-	$("#personal-location-input").val(localStorage.getItem('dataSavedLocation'));
-	$("#personal-link-input").val(localStorage.getItem('dataSavedUrl'));
-
-	$("#personal_statement_input").val(localStorage.getItem('dataSavedStatement'));
-
-	$("#employer").val(localStorage.getItem('dataSavedEmp'));
-	$("#job_location").val(localStorage.getItem('dataSavedJobLoc'));
-	$("#job_time").val(localStorage.getItem('dataSavedJobTime'));
-	$("#employer_desc").val(localStorage.getItem('dataSavedEmpDesc'));
-	$("#title").val(localStorage.getItem('dataSavedJobTitle'));
-	$("#job_desc").val(localStorage.getItem('dataSavedJobDesc'));
-
-	$("#degree").val(localStorage.getItem('dataSavedDegree'));
-	$("#school").val(localStorage.getItem('dataSavedSchool'));
-	$("#school_location").val(localStorage.getItem('dataSavedSchoolLoc'));
-	$("#graduation").val(localStorage.getItem('dataSavedGrad'));
-
-	$("#skillsinput").val(localStorage.getItem('dataSavedSkills'));
-}
-
-$("#clear_localstorage").click(function(){
-	localStorage.clear();
-	location.reload();
-})
-
-user_job_location = "";
-user_job_time = "";
-user_emp_desc = "";
-user_job_title = "";
-user_job_desc = "";
-user_employer2 = "";
-user_job_location2 = "";
-user_job_time2 = "";
-user_emp_desc2 = "";
-user_job_title2 = "";
-user_job_desc2 = "";
-user_employer3 = "";
-user_job_location3 = "";
-user_job_time3 = "";
-user_emp_desc3 = "";
-user_job_title3 = "";
-user_job_desc3 = "";
-user_degree = "";
-user_school = "";
-user_school_location = "";
-user_school_grad = "";
-user_degree2 = "";
-user_school2 = "";
-user_school_location2 = "";
-user_school_grad2 = "";
-user_degree3 = "";
-user_school3 = "";
-user_school_location3 = "";
-user_school_grad3 = "";
-
