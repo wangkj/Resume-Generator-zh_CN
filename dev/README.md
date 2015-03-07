986b4dcba980046401809f4d3659d79435cd2e11
Mitch Samuels <mitch@mitchs.co>
2015-03-06 21:50:47 -0600

    Lots of changes
    Progress bar
    
    HTML Preview
    
    Confirmation to clear data
    
    Dev branch
    
    I forgot what else.

---
+++ dev/README.md
@@ -0,0 +1,16 @@
+# Resume-Generator
+A simple free resume generator that's currently hosted on http://ineedaresu.me
+
+###Features
+<ul>
+  <li>Outputs as PDF or direct print</li>
+  <li>No data sent over servers</li>
+  <li>Uses HTML5 LocalStorage so users can come back and finish</li>
+  <li>Customization Settings, more coming soon</li>
+  <li><b>Works best/fully on Chrome on desktop.</b></li>
+</ul>
+
+##Changes
+I change too much stuff, so I'm not going to explain much because it'll most likely just change. I apologize for any inefficient Javascript or CSS :))
+
+This was made in the span of 4-5 hours between classes one day. It's not perfect, but it's free, and I'm continuing work on it.
---
+++ dev/src/tests.js
@@ -0,0 +1,69 @@
+/*
+
+███████╗██████╗ ██╗████████╗    ███╗   ███╗███████╗       ██╗ 
+██╔════╝██╔══██╗██║╚══██╔══╝    ████╗ ████║██╔════╝    ██╗╚██╗
+█████╗  ██║  ██║██║   ██║       ██╔████╔██║█████╗      ╚═╝ ██║
+██╔══╝  ██║  ██║██║   ██║       ██║╚██╔╝██║██╔══╝      ██╗ ██║
+███████╗██████╔╝██║   ██║       ██║ ╚═╝ ██║███████╗    ╚═╝██╔╝
+╚══════╝╚═════╝ ╚═╝   ╚═╝       ╚═╝     ╚═╝╚══════╝       ╚═╝ 
+  
+Edit this JS file to change any data you want displayed on the PDF
+
+*/
+
+function loadTestData(){
+
+	//Uncomment below and you can erase this line
+	$("#pdf_job2, #pdf_job3").hide();
+
+
+	//PDF Testing, filler info
+	$("#pdf_name").html("John A. Smith");
+	$("#pdf_email").html("JohnSmith@example.com");
+	$("#pdf_phone").html("507.555.5897");
+	$("#pdf_location").html("Madison, WI");
+	$("#pdf_link").html("<a href='#'>http://linkedin.com/in/johns</a>");
+	$("#pdf_statement_title").html("Professional Summary");
+	$("#pdf_statement").html("Bacon ipsum dolor amet tenderloin ball tip porchetta pork loin, strip steak fatback picanha shankle swine rump boudin biltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon. Picanha meatball sausage, spare ribs cow ham hock short loin. Ball tip ribeye short ribs fatback chicken ground round pork chop short loin beef ribs kevin landjaeger ham tri-tip. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly.");
+	
+	$("#job_1_employer").html("Google, Inc.");
+	$("#job_1_loc").html("Mountain View, California");
+	$("#job_1_time").html("July 2012 - May 2014");
+	$("#pdf_job1_emp_desc").html("Google is an American multinational corporation specializing in Internet-related services and products. These include online advertising technologies, search, cloud computing, and software.");
+	$("#job1_title").html("Front End Developer");
+	$("#pdf_job1_job_desc").html("Bacon ipsum dolor amet tenderloin ball tip porchetta pork loin, strip <a href='#'>steak fatback</a> picanha shankle swine rump boudin biltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon.<br/><b>Responsibilities Included</b><ul><li>Bacon ipsum dolor amet tenderloin ball tip porchetta pork loin, strip steak fatback picanha shankle</li><li>wine rump boudin biltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket.</li><li>Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump</li></ul>Abiltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon.");
+	
+	// $("#job_2_employer").html("Google, Inc.");
+	// $("#job_2_loc").html("Mountain View, California");
+	// $("#job_2_time").html("July 2012 - May 2014");
+	// $("#pdf_job2_emp_desc").html("Google is an American multinational corporation specializing in Internet-related services and products. These include online advertising technologies, search, cloud computing, and software.");
+	// $("#job2_title").html("Front End Developer");
+	// $("#pdf_job2_job_desc").html("Bacon ipsum dolor amet tenderloin ball tip porchetta pork loin, strip <a href='#'>steak fatback</a> picanha shankle swine rump boudin biltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon.<br/><b>Responsibilities Included</b><ul><li>Bacon ipsum dolor amet tenderloin ball tip porchetta pork loin, strip steak fatback picanha shankle</li><li>wine rump boudin biltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket.</li><li>Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump</li></ul>Abiltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon.");
+	
+	// $("#job_3_employer").html("Google, Inc.");
+	// $("#job_3_loc").html("Mountain View, California");
+	// $("#job_3_time").html("July 2012 - May 2014");
+	// $("#pdf_job3_emp_desc").html("Google is an American multinational corporation specializing in Internet-related services and products. These include online advertising technologies, search, cloud computing, and software.");
+	// $("#job3_title").html("Front End Developer");
+	// $("#pdf_job3_job_desc").html("Bacon ipsum dolor amet tenderloin ball tip porchetta pork loin, strip <a href='#'>steak fatback</a> picanha shankle swine rump boudin biltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon.<br/><b>Responsibilities Included</b><ul><li>Bacon ipsum dolor amet tenderloin ball tip porchetta pork loin, strip steak fatback picanha shankle</li><li>wine rump boudin biltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket.</li><li>Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump</li></ul>Abiltong doner shoulder. Meatball flank cow, short loin chuck cupim pork loin picanha kielbasa corned beef sirloin doner brisket. Ham hock bresaola porchetta beef ribs capicola. Alcatra pancetta jerky frankfurter corned beef, pork belly andouille pig rump bacon.");
+	
+
+
+	$("#pdf_degree").html("Bachelor of Science in Computer Science");
+	$("#pdf_school").html("University of Wisconsin-Madison");
+	$("#pdf_school_loc").html("Madison, WI");
+	$("#pdf_graduation").html("2017");
+	$("#pdf_degree2").html("Bachelor of Arts in Communications");
+	$("#pdf_school2").html("Berekely University");
+	$("#pdf_school_loc2").html("Berkely, CA");
+	$("#pdf_graduation2").html("2012");
+	$("#pdf_degree3").html("Doctor of Philosophy in Organizational Psychology");
+	$("#pdf_school3").html("University of Minnesota-Twin Cities");
+	$("#pdf_school_loc3").html("Minneapolis, MN");
+	$("#pdf_graduation3").html("2014");
+
+	user_skills="HTML, CSS, Javascript, PHP, Django, Python, Bootstrap, Wordpress, Angular, Ionic, Java, Photoshop, Indesign, Dketch, Evernote, Git/Github"
+	user_skills_split = user_skills.replace(/,/g, '<span class="skillbullet"> \u2219 </span>');
+	$("#pdf_skills").html(user_skills_split);
+
+}
